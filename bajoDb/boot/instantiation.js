fca3ca69ab3a37243a3fd4323e3ef1f3eed2b682
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-16 07:11:33 +0700

    Updates

--- bajoDb/boot/instantiation.js
+++ bajoDb/boot/instantiation.js
@@ -1,13 +1,13 @@
 async function instantiation ({ connection, schemas, noRebuild }) {
   const { importPkg } = this.bajo.helper
   const { pick } = await importPkg('lodash-es')
-  this.bajoDbMingo.storage = this.bajoDbMingo.storage || {}
+  this.bajoDbMingo.storage = this.bajoDbMingo.storage ?? {}
   const instance = pick(connection, ['name', 'type'])
-  this.bajoDbMingo.instances = this.bajoDbMingo.instances || []
+  this.bajoDbMingo.instances = this.bajoDbMingo.instances ?? []
   this.bajoDbMingo.instances.push(instance)
   // if (noRebuild) return
   for (const schema of schemas) {
-    this.bajoDbMingo.storage[schema.name] = this.bajoDbMingo.storage[schema.name] || [] // init empty coll
+    this.bajoDbMingo.storage[schema.name] = this.bajoDbMingo.storage[schema.name] ?? [] // init empty coll
   }
 }
 
