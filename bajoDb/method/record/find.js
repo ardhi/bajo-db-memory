b92f5d38bf24350f6d4b43d313af35630c4f3351
Ardhi Lukianto <ardhi@lukianto.com>
2023-11-13 21:29:57 +0700

    Updates

--- bajoDb/method/record/find.js
+++ bajoDb/method/record/find.js
@@ -2,12 +2,13 @@ import { Query } from 'mingo'
 
 async function find ({ schema, filter = {}, options = {} } = {}) {
   const { prepPagination } = this.bajoDb.helper
-  const { noLimit } = options
-  const { limit, skip, query, sort, page } = await prepPagination(filter, schema)
+  const { noLimit, dataOnly } = options
+  const { limit, skip, query, sort, page, noCount } = await prepPagination(filter, schema)
   const criteria = query ?? {}
   const q = new Query(criteria, { idKey: 'id' })
   let cursor = q.find(this.bajoDbMingo.storage[schema.name])
-  const count = cursor.count()
+  let count = 0
+  if (!noCount && !dataOnly) count = cursor.count()
   cursor = q.find(this.bajoDbMingo.storage[schema.name])
   if (sort) cursor.sort(sort)
   if (!noLimit) cursor.skip(skip).limit(limit)
